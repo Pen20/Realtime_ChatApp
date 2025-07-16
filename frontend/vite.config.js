dbb5cb1fab364e452a25efbeb237de804018c743
Wastalas <wastalasdassise@gmail.com>
2025-07-16 23:09:20 +0200

    Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  Base: process.env.VITE_BASE_PATH || "/Realtime_ChatApp/tree/main",
+});
